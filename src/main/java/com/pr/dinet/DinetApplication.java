c885c9cf730f95c39704f10a6ceccd48ea1f4982
reballesteros19 <reballesteros19@gmail.com>
2024-01-23 10:48:37 -0500

    first

---
+++ src/main/java/com/pr/dinet/DinetApplication.java
@@ -0,0 +1,13 @@
+package com.pr.dinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pr/dinet/DinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pr.dinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
