5d2dda7a6e40c1a9fa97330c156a6e6a415ecd56
rbalopez19 <ricardo.ballesteros@globant.com>
2024-01-29 10:17:09 -0500

    Inicio

--- src/main/resources/db/migration/V2__ddl.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE persona
-(
-    id uuid DEFAULT gen_random_uuid(),
-    apellido varchar(255),
-    nombre varchar(255),
-    telefono varchar(255),
-    PRIMARY KEY (id)
-)
--- src/main/resources/db/migration/V3__data.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO persona
-            (
-             apellido,
-             nombre,
-             telefono)
-VALUES      (
-              'apellido_2',
-              'nombre_2',
-              'telefono_2');
